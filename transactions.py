ae421894c49a68d79548eb3855a1c3980d7f0e6f
marina <torresgmarina@gmail.com>
2019-06-12 18:04:22 -0500

    tx immutable with correct scripts. Simulations work ok

--- transactions.py
+++ transactions.py
@@ -19,9 +19,9 @@ from collections import defaultdict
 from bitcoin import core
 from bitcoin import params
 from bitcoin.core import *
-from bitcoin.core.script import *
 from blockchain import pushtx
 from bitcoin import SelectParams
+from bitcoin.base58 import *
 from bitcoin.core import b2x, lx, COIN, COutPoint, CMutableTxOut, CMutableTxIn, CMutableTransaction, Hash160
 from bitcoin.core.script import *
 from bitcoin.core.scripteval import VerifyScript, SCRIPT_VERIFY_P2SH, OP_NOP3
@@ -30,16 +30,17 @@ from bitcoin.wallet import CBitcoinAddress, CBitcoinSecret
 SelectParams('regtest')
 
 
-def fundingTx(seckeyA, seckeyB, pk_A_FT, pk_B_FT, address1, address2, txin, value_in): 
+def fundingTx(seckeyA, seckeyB, pk_A_FT, pk_B_FT, txin, value_in): 
 
     # Generate redeem script and locking script
     r_S = CScript([OP_2, pk_A_FT, pk_B_FT, OP_2, OP_CHECKMULTISIG])
     r_S_Hash = Hash160(r_S)
     l_S = CScript([OP_HASH160,r_S_Hash, OP_EQUAL])
     
-    # Create transaction with txin and txout 
-    txout = CMutableTxOut(value_in-0.0005*COIN, CBitcoinAddress(address2).to_scriptPubKey())
-    tx = CMutableTransaction([txin], [txout])
+    # Create Transaction
+    txout = CTxOut(value_in-0.001*COIN,r_S)
+    print("TXOUT: ",txout)
+    tx = CTransaction([txin], [txout])
 
     # Create signatures
     sighash = SignatureHash(r_S, tx, 0, SIGHASH_ALL)
@@ -48,60 +49,57 @@ def fundingTx(seckeyA, seckeyB, pk_A_FT, pk_B_FT, address1, address2, txin, valu
     
     # Create unlocking script
     u_S = CScript([OP_0, sigA, sigB, r_S, l_S])
-
-    # Generate transaction script 
-    txin.scriptSig = CScript([sigA,sigB, r_S])
     
-    # Verify multisignature
-    VerifyScript(txin.scriptSig, l_S, tx, 0, (OP_CHECKMULTISIGVERIFY,))    
-    return u_S, l_S, r_S, tx
+    # Verify Script
+    scriptSig = CScript([sigA,sigB, r_S])
+    VerifyScript(scriptSig, l_S, tx, 0, (OP_CHECKMULTISIGVERIFY,))    
+    return u_S, l_S, r_S, tx, r_S_Hash 
 
-def closingTx(txid,seckeyA, seckeyB, pk_A_Close, pk_B_Close, address1, address2, value_in2):
+def closingTx(txid,seckeyA1, seckeyB1,seckeyA2, seckeyB2, r_S_Hash, pk_A_Close, pk_B_Close, value_in2):
     
-   # txid = tx_FT.GetTxid()
-    vout = 0
-    
-    # Cambiar esto para que haya dos outputs. Y que utilice los coins de la funding transaction. 
-    txin = CMutableTxIn(COutPoint(txid, vout))
-
+    # Create Locling Scripts
     l_SA = CScript([OP_DUP, OP_HASH160, Hash160(pk_A_Close), OP_EQUALVERIFY, OP_CHECKSIG])
     l_SB = CScript([OP_DUP, OP_HASH160, Hash160(pk_B_Close), OP_EQUALVERIFY, OP_CHECKSIG])
-    txout1 = CMutableTxOut(value_in2-0.0005*COIN-0.0005*COIN, CBitcoinAddress(address1).to_scriptPubKey())
-    txout2 = CMutableTxOut(0.0005*COIN, CBitcoinAddress(address2).to_scriptPubKey())
-    tx = CMutableTransaction([txin], [txout1,txout2])
     
+    # Create Transaction
+    vout = 0    
+    txout1 = CTxOut(value_in2 - 0.005*COIN,CScript([l_SA]))
+    txout2 = CTxOut(0.001*COIN,CScript([l_SB]))
+    txout = [txout1, txout2]
+    txin = CTxIn(COutPoint(txid, vout), CScript([seckeyA1,seckeyB1]))
+    tx = CTransaction([txin], txout)
+    print("closingTX: ", tx)
+    
+    # Create Unlocking Scripts
     sighashA = SignatureHash(l_SA, tx, 0, SIGHASH_ALL)
     sighashB = SignatureHash(l_SB, tx, 0, SIGHASH_ALL)
-    sigA = seckeyA.sign(sighashA) + bytes([SIGHASH_ALL])
-    sigB = seckeyB.sign(sighashB) + bytes([SIGHASH_ALL])
+    sigA = seckeyA2.sign(sighashA) + bytes([SIGHASH_ALL])
+    sigB = seckeyB2.sign(sighashB) + bytes([SIGHASH_ALL])
     sigs = []
     sigs.append(sigA)
     sigs.append(sigB)
     
-    u_SA = CScript([sigA, pk_A_Close])    
-    u_SB = CScript([sigB, pk_B_Close])
+    u_SA1 = CScript([sigA, pk_A_Close])    
+    u_SB1 = CScript([sigB, pk_B_Close])
 
+    # Verify Scripts
     for sig in sigs:
         if (sig == sigA):
-            seckey = seckeyA
-            txin.scriptSig = CScript([sig, seckey.pub])
-            VerifyScript(txin.scriptSig, l_SA, tx, 0, (SCRIPT_VERIFY_P2SH,))
+            seckey = seckeyA2
+            scriptSig = CScript([sig, seckey.pub])
+            VerifyScript(scriptSig, l_SA, tx, 0, (SCRIPT_VERIFY_P2SH,))
             
         elif(sig == sigB):
-            seckey = seckeyB
-            txin.scriptSig = CScript([sig, seckey.pub])
-            VerifyScript(txin.scriptSig, l_SB, tx, 0, (SCRIPT_VERIFY_P2SH,))
-              
-    return u_SA, u_SB, l_SA, l_SB, tx
+            seckey = seckeyB2
+            scriptSig = CScript([sig, seckey.pub])
+            VerifyScript(scriptSig, l_SB, tx, 0, (SCRIPT_VERIFY_P2SH,))
+    
+    return u_SA1, u_SB1, l_SA, l_SB, tx
             
-def commitmentA(tx_FT,tx_index, devices_keypairs, third_a_keypairs, address1, address2):
-    txid = tx_FT.GetTxid()
+def commitmentA(ft_id,tx_index, devices_keypairs, third_a_keypairs, value_in2):
+    txid = ft_id
     vout = 0
     txin = CMutableTxIn(COutPoint(txid, vout))
-    
-    # PREGUNTA: QUÉ BITCOIN ADDRESS HAY QUE PONER !???
-    txout = CMutableTxOut(0.001*COIN, CBitcoinAddress(address1).to_scriptPubKey())
-    tx = CMutableTransaction([txin], [txout])
     
     # Devices keys
         #Indexes: [x][y][z]
@@ -141,6 +139,13 @@ def commitmentA(tx_FT,tx_index, devices_keypairs, third_a_keypairs, address1, ad
     
     # Output 3
     l_S3 = CScript([OP_1, pk_3_a_0, pk_3_a_1, OP_2, OP_CHECKMULTISIG])
+    
+    txin = CTxIn(COutPoint(txid, vout))    
+    txout1 = CTxOut(value_in2 - 0.005*COIN,CScript([l_SA]))
+    txout2 = CTxOut(0.001*COIN,CScript([l_SB]))
+    txout3 = CTxOut(0.001*COIN,CScript([l_S3]))
+    txout = [txout1, txout2, txout3]
+    tx = CTransaction([txin], txout)
 
     sighashA = SignatureHash(l_SA, tx, 0, SIGHASH_ALL)
     sighashB = SignatureHash(l_SB, tx, 0, SIGHASH_ALL)
@@ -157,29 +162,24 @@ def commitmentA(tx_FT,tx_index, devices_keypairs, third_a_keypairs, address1, ad
     for sig in sigs:
         if (sig == sigA):
             seckey = sk_A_a
-            txin.scriptSig = CScript([sig, seckey.pub])
-            VerifyScript(txin.scriptSig, l_SA, tx, 0, (SCRIPT_VERIFY_P2SH,))
+            scriptSig = CScript([sig, seckey.pub])
+            VerifyScript(scriptSig, l_SA, tx, 0, (SCRIPT_VERIFY_P2SH,))
             
         elif(sig == sigB):
             seckey = sk_B_b
-            txin.scriptSig = CScript([sig, seckey.pub])
-            VerifyScript(txin.scriptSig, l_SB, tx, 0, (SCRIPT_VERIFY_P2SH,))
+            scriptSig = CScript([sig, seckey.pub])
+            VerifyScript(scriptSig, l_SB, tx, 0, (SCRIPT_VERIFY_P2SH,))
         
         elif(sig == sig3_0):
             seckey = sk_3_b_0
-            txin.scriptSig = CScript([0, sig])
-            VerifyScript(txin.scriptSig, l_S3, tx, 0, (OP_CHECKMULTISIGVERIFY,))
+            scriptSig = CScript([0, sig])
+            VerifyScript(scriptSig, l_S3, tx, 0, (OP_CHECKMULTISIGVERIFY,))
+
 
     return l_SA, l_SB, l_S3, tx
 
-def commitmentB(tx_FT,tx_index, devices_keypairs, third_b_keypairs, address1, address2):
-    txid = tx_FT.GetTxid()
-    vout = 0
-    txin = CMutableTxIn(COutPoint(txid, vout))
-    
-    txout = CMutableTxOut(0.001*COIN, CBitcoinAddress(address2).to_scriptPubKey())
-    tx = CMutableTransaction([txin], [txout]) 
-    
+def commitmentB(ft_id,tx_index, devices_keypairs, third_b_keypairs, value_in2):
+           
     # Devices keys
         #Indexes: [x][y][z]
         # x: 0= IoT (device A), 1 = Gateway (device B)
@@ -216,6 +216,14 @@ def commitmentB(tx_FT,tx_index, devices_keypairs, third_b_keypairs, address1, ad
     
     # Output 2
     l_SA = CScript([OP_DUP, OP_HASH160, Hash160(pk_A_b), OP_EQUALVERIFY, OP_CHECKSIG])
+    
+    txid = ft_id
+    vout = 0 
+    txin = CTxIn(COutPoint(txid, vout)) 
+    txout1 = CTxOut(value_in2 - 0.005*COIN,CScript([l_SA]))
+    txout2 = CTxOut(0.001*COIN,CScript([l_SB]))
+    txout = [txout1, txout2]
+    tx = CTransaction([txin], txout)
         
     # Verify Scripts
     sighashA = SignatureHash(l_SA, tx, 0, SIGHASH_ALL)
@@ -232,24 +240,17 @@ def commitmentB(tx_FT,tx_index, devices_keypairs, third_b_keypairs, address1, ad
     for sig in sigs:
         if (sig == sigA):
             seckey = sk_A_b
-            txin.scriptSig = CScript([sig, seckey.pub])
-            # Does P2SH makes sense here?
-            VerifyScript(txin.scriptSig, l_SA, tx, 0, (SCRIPT_VERIFY_P2SH,))
+            scriptSig = CScript([sig, seckey.pub])
+            VerifyScript(scriptSig, l_SA, tx, 0, (SCRIPT_VERIFY_P2SH,))
             
         if(sig == sigB):
             seckey = sk_B_a
-            txin.scriptSig = CScript([sig, seckey.pub])
-            VerifyScript(txin.scriptSig, l_SB, tx, 0, (SCRIPT_VERIFY_P2SH,))
+            scriptSig = CScript([sig, seckey.pub])
+            VerifyScript(scriptSig, l_SB, tx, 0, (SCRIPT_VERIFY_P2SH,))
 
     return l_SA, l_SB, tx, sigA, sigC
  
 def recoveryTx(tx_commB,tx_index, l_SB, devices_keypairs,third_a_keypairs, third_b_keypairs, address2, sigA, sigC):
- 
-    txid = tx_commB.GetTxid()
-    vout = 0
-    txin = CMutableTxIn(COutPoint(txid, vout))
-    txout = CMutableTxOut(0.001*COIN, CBitcoinAddress(address2).to_scriptPubKey())
-    tx = CMutableTransaction([txin], [txout])
     
     # Devices keys and signatures
     pk_A_b = devices_keypairs[tx_index][0][1][0]
@@ -286,25 +287,36 @@ def recoveryTx(tx_commB,tx_index, l_SB, devices_keypairs,third_a_keypairs, third
     
     # Out 1
     l_S1 = CScript([OP_DUP, OP_HASH160, Hash160(pk_A_rc), OP_EQUALVERIFY, OP_CHECKSIG])     
-    sighashA = SignatureHash(l_S1, tx, 0, SIGHASH_ALL)
+    sighashA = SignatureHash(l_S1, tx_commB, 0, SIGHASH_ALL)
     sig_A_rc = sk_A_rc.sign(sighashA) + bytes([SIGHASH_ALL])    
     
     u_S1 = CScript([sig_A_rc, pk_A_rc])
     
     # Out 2
     l_S2 = CScript([OP_DUP, OP_HASH160, Hash160(pk_3_b_omega), OP_EQUALVERIFY, OP_CHECKSIG])
-    sighash = SignatureHash(l_S2, tx, 0, SIGHASH_ALL)
+    sighash = SignatureHash(l_S2, tx_commB, 0, SIGHASH_ALL)
     sig_3_b_omega = sk_3_b_omega.sign(sighash) + bytes([SIGHASH_ALL])  
 
+    # Out 3
     l_S3 = CScript([OP_1, pk_3_a_0, pk_3_a_1, OP_2, OP_CHECKMULTISIG, OP_DROP])     
 
+    # Create tx
+    txid = tx_commB.GetTxid()
+    vout = 0
+    txin = CTxIn(COutPoint(txid, vout))   
+    txout1 = CTxOut( 0.005*COIN,CScript([l_S1, u_S1]))
+    txout2 = CTxOut(0.001*COIN,CScript([l_S2]))
+    txout3 = CTxOut(0.001*COIN,CScript([l_S3]))
+    txout = [txout1, txout2, txout3]
+    tx = CTransaction([txin], txout)
+    
     # Verify Scripts 
-    txin.scriptSig = CScript([sig_A_rc, sk_A_rc.pub]) 
-    VerifyScript(txin.scriptSig, l_S1, tx, 0, (SCRIPT_VERIFY_P2SH,))
+    scriptSig = CScript([sig_A_rc, sk_A_rc.pub]) 
+    VerifyScript(scriptSig, l_S1, tx_commB, 0, (SCRIPT_VERIFY_P2SH,))
     
-    txin.scriptSig = CScript([sig_3_b_omega, sk_3_b_omega.pub]) 
-    VerifyScript(txin.scriptSig, l_S2, tx, 0, (SCRIPT_VERIFY_P2SH,))
-    
+    scriptSig = CScript([sig_3_b_omega, sk_3_b_omega.pub]) 
+    VerifyScript(scriptSig, l_S2, tx_commB, 0, (SCRIPT_VERIFY_P2SH,))
+  
     return l_S1, u_S1, l_S2, l_S3, tx
     
 # Generate a random string of fixed length
@@ -322,70 +334,43 @@ def main():
     address1 = str(address1)
     address2 = proxy_connection.getnewaddress()
     address2 = str(address2)
-    
-    address3 = proxy_connection.getnewaddress()
-    address3 = str(address3)
-    
-    #### FUNDING TRANSACTION ####
-    # Get initial UTXO from wallet to fund the Funding Transaction
-    unspent = sorted(proxy_connection.listunspent(0), key=lambda x: hash(x['amount']))
-    txin = [CMutableTxIn(unspent[-1]['outpoint'])][0]
-    value_in = unspent[-1]['amount']
-    
-  
-    # Create key pairs for Funding Transaction
-    hA1 = hashlib.sha256(b'correct horse battery staple').digest()
-    hB1 = hashlib.sha256(b'sdfdsfdsdfsd').digest()
-    seckeyA1 = CBitcoinSecret.from_secret_bytes(hA1)
-    seckeyB1 = CBitcoinSecret.from_secret_bytes(hB1)
+    seckeyA1 = proxy_connection.dumpprivkey(address1)
+    seckeyB1 = proxy_connection.dumpprivkey(address2)
     pk_A_FT = seckeyA1.pub
     pk_B_FT = seckeyB1.pub
+    #### FUNDING TRANSACTION ####
+    
+    # Get initial UTXO from wallet to fund the Funding Transaction
+    unspent = sorted(proxy_connection.listunspent(0), key=lambda x: hash(x['amount']))
+    txin = [CTxIn(unspent[-1]['outpoint'])][0]
+    value_in = unspent[-1]['amount']
+      
+    # Create key pairs for Funding Transaction
+    #hA1 = hashlib.sha256(b'correct horse battery staple').digest()
+    #hB1 = hashlib.sha256(b'sdfdsfdsdfsd').digest()
+    #seckeyA1 = CBitcoinSecret.from_secret_bytes(hA1) 
+    #seckeyB1 = CBitcoinSecret.from_secret_bytes(hB1) 
+    #pk_A_FT = seckeyA1.pub
+    #pk_B_FT = seckeyB1.pub
     
     # Perform Funding Transaction
-    u_S, l_S, r_S, tx_FT = fundingTx(seckeyA1, seckeyB1, pk_A_FT, pk_B_FT, address1, address2, txin,value_in)
-
+    u_S, l_S, r_S, tx_FT, r_S_Hash = fundingTx(seckeyA1, seckeyB1, pk_A_FT, pk_B_FT, txin, value_in)
     # Sign raw transaction and send it to the connected node
+    b58_secA = bitcoin.base58.encode(seckeyA1)
+    b58_secB = bitcoin.base58.encode(seckeyB1)
     r = proxy_connection.signrawtransaction(tx_FT)
-    tx = r['tx']
-    ft_id =proxy_connection.sendrawtransaction(tx)
+    tx0 = r['tx']
+    ft_id =proxy_connection.sendrawtransaction(tx0)
+
     print("Funding Transaction ID")
     print(b2lx(ft_id))
     print("\n")
     
     # Generate block to confirm transaction
-    proxy_connection.generate(1)
-    
-    # Verify that transaction has been added to the block. Need to review this (!!!)
-    nwInfo = proxy_connection.getmininginfo()
-    print("Mining info")
-    print(nwInfo)
-    print("\n")
-    
-    ##### CLOSING TRANSACTION #####
-    # Create key pairs for Closing Transaction
-    hA2 = hashlib.sha256(b'kjhgkhkq').digest()
-    hB2 = hashlib.sha256(b'asdfawei').digest()
-    seckeyA2 = CBitcoinSecret.from_secret_bytes(hA2)
-    seckeyB2 = CBitcoinSecret.from_secret_bytes(hB2)        
-    pk_A_Close = seckeyA2.pub
-    pk_B_Close = seckeyB2.pub
-    
-    # Get funds from previous Funding Transaction
-    unspent = sorted(proxy_connection.listunspent(0), key=lambda x: hash(x['amount']))
-    value_in2 = 0    
-    for i in range(len(unspent)):         
-        if(str(unspent[i]['outpoint'])==str(b2lx(ft_id)+":0")):
-            value_in2 = unspent[i]['amount']
-            
-    # Perform Closing Transaction
-    u_SA, u_SB, l_SA, l_SB, close_tx = closingTx(ft_id ,seckeyA2, seckeyB2, pk_A_Close, pk_B_Close, address1, address2, value_in2)
-
-    # Sign raw transaction and send it to the connected node
-    r = proxy_connection.signrawtransaction(close_tx)
-    tx = r['tx']
-    close_id =proxy_connection.sendrawtransaction(tx)
+    proxy_connection.generate(1) 
     
     #### COMMITMENT TRANSACTIONS ####
+    
     # Third Parties Key Pairs. There are two group of third parties (third_a and third_b). Each of them composed by 3 third parties.
     third_a_keypairs = []
     third_b_keypairs = []
@@ -436,23 +421,71 @@ def main():
             
             keys_device[j] = [[pk_a,sk_a],[pk_b,sk_b],[pk_c,sk_c],[pk_rc,sk_rc]]
             devices_keypairs[i][j] = keys_device[j]
-     
+    
     #### COMMITMENT TRANSACTION A ####
+    # Get funds from previous Funding Transaction
+    value_in2 = proxy_connection.getrawtransaction(ft_id).vout[0].nValue
+    
     # Index of the commitment transaction. Should range between 0 and 9. 
-    tx_index = 0
-    l_SA, l_SB, l_S3, tx_commA = commitmentA(tx_FT,tx_index, devices_keypairs, third_a_keypairs, address1, address2)
-     
-    #### COMMITMENT TRANSACTION B #### 
-    tx_index = 1
-    l_SA, l_SB, tx_commB, sigA, sigC = commitmentB(tx_FT,tx_index, devices_keypairs, third_b_keypairs, address1, address2)
-     
-    #### RECOVERY TRANSACTION #### 
+    tx_index0 = 0
+    l_SA, l_SB, l_S3, tx_commA0 = commitmentA(ft_id, tx_index0, devices_keypairs, third_a_keypairs, value_in2)
     
-    # Key pairs for A transacting with the Third Parties
-    
-    # Perform recovery Tx. I use tx_index to search for the keys used for commitment_B
-    l_S1, u_S1, l_S2, l_S3, tx_recov = recoveryTx(tx_commB,tx_index,l_SB, devices_keypairs,third_a_keypairs, third_a_keypairs, address2, sigA, sigC)
+    # Sign raw transaction and send it to the connected node
+    r = proxy_connection.signrawtransaction(tx_commA0)
+    tx = r['tx']
+    comm_A_id =proxy_connection.sendrawtransaction(tx) 
+    print("commitment A :" , b2x(comm_A_id))
+# =============================================================================
+#     
+#     ##### CLOSING TRANSACTION #####
+#     
+#     # Create key pairs for Closing Transaction
+#     hA2 = hashlib.sha256(b'kjhgkhkq').digest()
+#     hB2 = hashlib.sha256(b'asdfawei').digest()
+#     seckeyA2 = CBitcoinSecret.from_secret_bytes(hA2)
+#     seckeyB2 = CBitcoinSecret.from_secret_bytes(hB2)        
+#     pk_A_Close = seckeyA2.pub
+#     pk_B_Close = seckeyB2.pub
+#     
+#     # Get funds from previous Funding Transaction
+#     value_in2 = proxy_connection.getrawtransaction(ft_id).vout[0].nValue
+#             
+#     # Perform Closing Transaction
+#     u_SA, u_SB, l_SA, l_SB, close_tx = closingTx(ft_id, seckeyA1, seckeyB1, seckeyA2, seckeyB2, r_S_Hash, pk_A_Close, pk_B_Close, value_in2)
+#     
+#     # Sign raw transaction and send it to the connected node  
+#     r = proxy_connection.signrawtransaction(close_tx)
+#     print("R: ",r)
+#     print("\n")
+#     tx = r['tx']
+#     close_id =proxy_connection.sendrawtransaction(tx)
+#     
+#     # Generate block to confirm transaction
+#     proxy_connection.generate(1)
+#     
+#     print("Closing Transaction ID")
+#     print(b2lx(close_id))
+#     print("\n")
+# =============================================================================
 
+    
+    #### COMMITMENT TRANSACTION B #### 
+    tx_index1 = 1
+    # Get funds from previous Funding Transaction
+    value_in2 = proxy_connection.getrawtransaction(ft_id).vout[0].nValue
+    l_SA, l_SB, tx_commB, sigA, sigC = commitmentB(ft_id,tx_index1, devices_keypairs, third_b_keypairs, value_in2)
+
+# =============================================================================
+#     # Sign raw transaction and send it to the connected node
+#     r = proxy_connection.signrawtransaction(tx_commB)
+#     tx = r['tx']
+#     comm_B_id =proxy_connection.sendrawtransaction(tx)
+#     print(b2x(comm_A_id))
+# =============================================================================
+
+    #### RECOVERY TRANSACTION ####      
+    # Perform recovery Tx. I use tx_index to search for the keys used for commitment_B
+    l_S1, u_S1, l_S2, l_S3, tx_recov = recoveryTx(tx_commB,tx_index1,l_SB, devices_keypairs,third_a_keypairs, third_a_keypairs, address2, sigA, sigC)
 if __name__ == '__main__':
     main()
 
